93afeda6a2c9e76496e760ffe839a734d1b97859
lezhou <travis.baker24.th@gmail.com>
2023-04-15 23:18:34 +0800

    display in browser
    Signed-off-by: lezhou <travis.baker24.th@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,38 @@
-use std::net::SocketAddr;
-
-use axum::{Server, Router};
+use axum::extract::State;
 use axum::routing::get;
+use axum::{Router, Server};
+use std::net::SocketAddr;
+use std::sync::{Arc, Mutex};
+use sysinfo::{CpuExt, System, SystemExt};
 use tracing_subscriber;
 
 #[tokio::main]
 async fn main() {
     tracing_subscriber::fmt::init();
-    let router = Router::new().route("/", get(rootget));
+    let router = Router::new().route("/", get(rootget)).with_state(AppState {
+        sys: Arc::new(Mutex::new(System::new())),
+    });
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     tracing::debug!("");
-    Server::bind(&addr).serve(router.into_make_service())
-        .await.unwrap();
+    Server::bind(&addr)
+        .serve(router.into_make_service())
+        .await
+        .unwrap();
 }
-async fn rootget() -> &'static str {
-    "Hello from Axum"
+#[derive(Clone)]
+struct AppState {
+    sys: Arc<Mutex<System>>,
+}
+
+async fn rootget(State(state): State<AppState>) -> String {
+    use std::fmt::Write;
+    let mut s = String::new();
+    let mut sys = state.sys.lock().unwrap();
+    sys.refresh_cpu();
+    for (index, cpu) in sys.cpus().iter().enumerate() {
+        let i = index + 1;
+        let usage = cpu.cpu_usage();
+        writeln!(&mut s, "CPU {i} {usage} ").unwrap();
+    }
+    s
 }
