615dc6efbdcaf0e83674ff791035a84454e87e8e
lezhou <travis.baker24.th@gmail.com>
2023-04-16 22:49:27 +0800

    fixed to 2 digits

--- src/index.mjs
+++ src/index.mjs
@@ -1,14 +1,24 @@
 import {h, Component, render} from "https://unpkg.com/preact?module";
+import htm from "https://unpkg.com/htm?module"
 
-document.addEventListener("DOMContentLoaded", () => {
-    let i = 0;
-    setInterval(async () => {
-        let resp = await fetch("/api/cpus");
-        if (resp.status !== 200)  {
-            throw new Error(`Http error! status ${resp.status}`)
-        }
-        let json = await resp.json();
-        const app = h("pre", null, JSON.stringify(json, null, 2));
-        render(app, document.body);
-    }, 1000);
-});
+
+const html = htm.bind(h);
+function App(props) {
+    return html`
+    <div>
+        ${props.cpus.map((cpu) => {
+            return html`<div> ${cpu.toFixed(2)}% usage </div>`;
+        })}
+    </div>`;
+}
+
+let i = 0;
+setInterval(async () => {
+    let resp = await fetch("/api/cpus");
+    if (resp.status !== 200)  {
+        throw new Error(`Http error! status ${resp.status}`)
+    }
+    let json = await resp.json();
+    const app = h("pre", null, JSON.stringify(json, null, 2));
+    render(html`<${App} cpus=${json} usage></${App}>`, document.body);
+}, 1000);
